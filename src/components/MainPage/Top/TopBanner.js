e1d8cd6987ca285b1a4473f197f2b656db6f1879
KalinSav <48828687+KalinSav@users.noreply.github.com>
2019-07-03 16:54:12 +0100

    First steps into Search Bar funcitonality
    located in TopBanner.js

--- src/components/MainPage/Top/TopBanner.js
+++ src/components/MainPage/Top/TopBanner.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { NavLink, HashRouter } from "react-router-dom";
 
 class TopBanner extends React.Component {
+  componentDidMount() {
+    console.log("asd2");
+    const NavLinks = document.querySelectorAll(".topBanner a");
+    [].forEach.call(NavLinks, eachNavLink => {
+      eachNavLink.addEventListener("click", () => this.handleClick());
+    });
+  }
+
+  handleClick() {
+    console.log("asd");
+  }
   render() {
     return (
       <header>
@@ -8,6 +20,14 @@ class TopBanner extends React.Component {
         <div className="topBanner">
           <h1>International Students</h1>
           <h2>Online Learning Resource</h2>
+          <HashRouter>
+            <NavLink to="/Europe/Spain" replace>
+              Spain
+            </NavLink>
+            <NavLink to="/Europe/France" replace>
+              France
+            </NavLink>
+          </HashRouter>
         </div>
       </header>
     );
